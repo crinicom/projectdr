70b6c7faeaa1281454f2713e2ee6feffec38adc4
crinicom <cristian.menajovsky@gmail.com>
2018-01-08 16:13:15 -0300

    sigo Deploying your Sails app to Heroku de Ryan schuetzler

--- config/connections.js
+++ config/connections.js
@@ -75,13 +75,15 @@ module.exports.connections = {
   *                                                                          *
   *                                                                          *
   ***************************************************************************/
-  // somePostgresqlServer: {
-  //   adapter: 'sails-postgresql',
-  //   host: 'YOUR_POSTGRES_SERVER_HOSTNAME_OR_IP_ADDRESS',
-  //   user: 'YOUR_POSTGRES_USER', // optional
-  //   password: 'YOUR_POSTGRES_PASSWORD', // optional
-  //   database: 'YOUR_POSTGRES_DB' //optional
-  // }
+  heroku_postgres: {
+    adapter: 'sails-postgresql',
+   // host: 'YOUR_POSTGRES_SERVER_HOSTNAME_OR_IP_ADDRESS',
+   // user: 'YOUR_POSTGRES_USER', // optional
+   // password: 'YOUR_POSTGRES_PASSWORD', // optional
+   // database: 'YOUR_POSTGRES_DB' //optional
+   url: process.env.DATABASE_URL, // Automatically set by Heroku
+   ssl: false
+  }
 
 
   /***************************************************************************
