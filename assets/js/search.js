6a6032b44b7b77f3685384570572a09a63be2e66
crinicom <cristian.menajovsky@gmail.com>
2018-02-16 17:15:53 -0300

    cambio static.home y mejoro vista mis proyectos

--- assets/js/search.js
+++ assets/js/search.js
@@ -6,3 +6,12 @@ $(document).ready(function(){
       });
     });
   });
+
+  $(document).ready(function(){
+    $("#myInput2").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $("#myTable2 tr").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
+  });
