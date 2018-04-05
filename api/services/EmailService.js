3dc56c1cb8a291dacef1194fea063e0eca6da6af
Melissa Romina Gross Cerdán <Romina@MacBook-Pro-de-Melissa.local>
2018-04-05 01:12:25 -0300

    envia mailsgit add .

--- api/services/EmailService.js
+++ api/services/EmailService.js
@@ -3,11 +3,11 @@ module.exports.sendWelcomeMail = function(obj) {
     sails.hooks.email.send(
     'welcomeEmail', 
     {
-    Name: obj.name
+        Name: obj.name
     },
     {
-    to: obj.email,
-    subject: "Welcome Email"
+        to: obj.email,
+         subject: "Welcome Email"
     },
     function(err) {console.log(err || "Mail Sent!");}
     )
--- config/email.js
+++ config/email.js
@@ -1,8 +1,10 @@
 module.exports.email = {
     service: 'Mailgun',
     auth: {
-        api_key: "key-9ceb1387dc5130a7a830dd757f003f88",
-        domain: "sandbox239a897a5c494d2ca34c4a33944bfa15.mailgun.org",
+      user: "postmaster@sandbox239a897a5c494d2ca34c4a33944bfa15.mailgun.org",
+      pass: "f4188e12667eb874599cda9ff080716c-bdd08c82-17ecd52a"
+        //api_key: "key-9ceb1387dc5130a7a830dd757f003f88",
+        //domain: "sandbox239a897a5c494d2ca34c4a33944bfa15.mailgun.org",
       },
     templateDir: 'views/emailTemplates',
     from: 'info@mycompany.com',
