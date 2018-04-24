c24eb61855bc67fa69ca8870965dab5b535eee29
crinicom <cristian.menajovsky@gmail.com>
2018-04-24 10:06:36 -0300

    no logro salir por el puerto 587, probar en casa

--- config/email.js
+++ config/email.js
@@ -1,16 +1,16 @@
 module.exports.email = {
-    service: 'Mailgun',
-   // transporter: {
-     // host: 'smtp.gmail.com',
-      port: 2525,
-     // secure: true,
-    auth: {
+   service: 'Mailgun',
+   //transporter: {
+   /*   host: 'smtp.mailgun.org',
+    port: 2525,
+     secure: true,
+    */ auth: {
       user: "postmaster@sandbox239a897a5c494d2ca34c4a33944bfa15.mailgun.org",
       pass: "f4188e12667eb874599cda9ff080716c-bdd08c82-17ecd52a"
         //api_key: "key-9ceb1387dc5130a7a830dd757f003f88",
         //domain: "sandbox239a897a5c494d2ca34c4a33944bfa15.mailgun.org",
       },
-    //},
+    /* }, */
     templateDir: 'views/emailTemplates',
     from: 'cristian.menajovsky@gmail.com',
     testMode: false,
