7d81c43d12a5d077fdfa8a558e8c913c6a64a902
crinicom <cristian.menajovsky@gmail.com>
2018-12-30 07:50:11 -0300

    servicio setstate funciona!

--- api/services/StateService.js
+++ api/services/StateService.js
@@ -1,10 +1,11 @@
 module.exports.SetState = function(obj = {}) {
+    console.log("obj en setstate antes del set: ", obj);
     Project.findOne(obj.id, function foundProject(err, project) {
         if (err) return next(err);
         if (!project) return next();
         
-        project.status[obj.key] = obj.state;
-
+        project.status[0][obj.key] = obj.state;
+        console.log("status en setstate, despues del set: ", project.status);
         project.save(function(err){
             if (err) {
                 return err;
